c75e08d8a6469e5900641e9b79c3f2e1a843abbc
im-ravira <imravira@gmail.com>
2024-08-29 12:18:03 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='h-screen bg-black text-white flex justify-center items-center text-6xl'>Initial setup</h1>
+    </div>
+  )
+}
+
+export default App
