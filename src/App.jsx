e83c93295c3ba9a176ec1716fce4e4ebb157d9ae
im-ravira <imravira@gmail.com>
2024-09-28 21:35:43 +0530

    feat: rename TopAttraction to Attraction and TopDestinations to Destinations

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Destinations from "./pages/TopDestinations";
-import TopAttractions from "./pages/TopAttractions";
+import Destinations from "./pages/Destinations";
+import Attractions from "./pages/Attractions";
 import MaintenancePage from "./pages/MaintenancePage";
 import TravelEssentials from "./pages/TravelEssentials";
 import Map from "./pages/Map";
@@ -16,8 +16,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/destinations" element={<Destinations />} />
-        <Route path="/attractions" element={<TopAttractions />} />
-        <Route path="travel-essentials" element={<TravelEssentials />} />
+        <Route path="/attractions" element={<Attractions />} />
+        <Route path="/travel-essentials" element={<TravelEssentials />} />
         <Route path="/map" element={<Map />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
