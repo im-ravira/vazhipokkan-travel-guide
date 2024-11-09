fbd4b43e6ed918e0f9b7757b0ca5137280de8a08
im-ravira <imravira@gmail.com>
2024-11-09 15:10:04 +0530

    chore: update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Set the output directory
-    rollupOptions: {
-      // You can add Rollup options here if needed
-    }
+    outDir: 'dist',
   },
   server: {
     port: 7000,
