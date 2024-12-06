b43c25b2ec5a7c98bd00de6af7d760353d4e72e9
im-ravira <imravira@gmail.com>
2024-12-06 23:02:46 +0530

    Fix incorrect URLs in generate-sitemap script

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -2,22 +2,23 @@ import { writeFileSync } from 'fs';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const urls = [
-    'https://https://vazhipokkan.netlify.app/',
-    'https://https://vazhipokkan.netlify.app/about',
-    'https://https://vazhipokkan.netlify.app/attractions',
-    'https://https://vazhipokkan.netlify.app/contact',
-    'https://https://vazhipokkan.netlify.app/destinations',
-    'https://https://vazhipokkan.netlify.app/gallery',
-    'https://https://vazhipokkan.netlify.app/map',
-    'https://https://vazhipokkan.netlify.app/travel-essentials',
+    'https://vazhipokkan.netlify.app/',
+    'https://vazhipokkan.netlify.app/about',
+    'https://vazhipokkan.netlify.app/attractions',
+    'https://vazhipokkan.netlify.app/contact',
+    'https://vazhipokkan.netlify.app/destinations',
+    'https://vazhipokkan.netlify.app/destinations/madurai',
+    'https://vazhipokkan.netlify.app/gallery',
+    'https://vazhipokkan.netlify.app/map',
+    'https://vazhipokkan.netlify.app/travel-essentials',
 ];
 
 const generateSitemap = () => {
   const sitemapContent = `
+    <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${urls
         .map(
@@ -35,4 +36,5 @@ const generateSitemap = () => {
 };
 
 generateSitemap();
+
 
